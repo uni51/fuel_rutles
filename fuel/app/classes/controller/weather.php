c4413cd54eaa89e394a57166456b9b9820d014ac
uni51 <uni51@users.noreply.github.com>
2020-01-24 13:33:02 +0900

    6.2.6 Restコントローラ

---
+++ fuel/app/classes/controller/weather.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * サンプルRestコントローラ
+ */
+
+class Controller_Weather extends Controller_Rest
+{
+    public function get_today()
+    {
+        $location = Input::get('loc');
+        $weather = 'fine';
+
+        $this->response(array(
+            'location' => $location,
+            'weather' => $weather,
+        ));
+    }
+}
+
+
